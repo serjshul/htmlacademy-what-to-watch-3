3cdfb24439d8163d5bc4f4ea7af2e59ed5d056e4
serjshul <shulginsergey0@gmail.com>
2024-01-15 16:17:05 +0300

    Added component sign-in-screen

---
+++ src/pages/sign-in-screen/sign-in-screen.tsx
@@ -0,0 +1,63 @@
+export default function SignInScreen() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8"/>
+        <title>WTW</title>
+        <meta name="robots" content="noindex, nofollow"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="css/main.min.css"/>
+      </head>
+
+      <body>
+        <div className="visually-hidden"></div>
+
+        <div className="user-page">
+          <header className="page-header user-page__head">
+            <div className="logo">
+              <a href="main.html" className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <h1 className="page-title user-page__title">Sign in</h1>
+          </header>
+
+          <div className="sign-in user-page__content">
+            <form action="#" className="sign-in__form">
+              <div className="sign-in__fields">
+                <div className="sign-in__field">
+                  <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+                  <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+                </div>
+                <div className="sign-in__field">
+                  <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+                  <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+                </div>
+              </div>
+              <div className="sign-in__submit">
+                <button className="sign-in__btn" type="submit">Sign in</button>
+              </div>
+            </form>
+          </div>
+
+          <footer className="page-footer">
+            <div className="logo">
+              <a href="main.html" className="logo__link logo__link--light">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <div className="copyright">
+              <p>© 2019 What to watch Ltd.</p>
+            </div>
+          </footer>
+        </div>
+      </body>
+    </html>
+  );
+}
