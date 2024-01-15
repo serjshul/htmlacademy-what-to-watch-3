40c3b557fa65931ca8224a1bb81ab165926d7ac6
serjshul <shulginsergey0@gmail.com>
2024-01-15 16:25:14 +0300

    Added component player-screen

---
+++ src/pages/player-screen/player-screen.tsx
@@ -0,0 +1,50 @@
+export default function PlayerScreen() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8"/>
+        <title>WTW</title>
+        <meta name="robots" content="noindex, nofollow"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="css/main.min.css"/>
+      </head>
+
+      <body>
+        <div className="visually-hidden"></div>
+
+        <div className="player">
+          <video src="#" className="player__video" poster="img/player-poster.jpg"></video>
+
+          <button type="button" className="player__exit">Exit</button>
+
+          <div className="player__controls">
+            <div className="player__controls-row">
+              <div className="player__time">
+                <progress className="player__progress" value="30" max="100"></progress>
+                <div className="player__toggler" style={{left: 30}}>Toggler</div>
+              </div>
+              <div className="player__time-value">1:30:29</div>
+            </div>
+
+            <div className="player__controls-row">
+              <button type="button" className="player__play">
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s"></use>
+                </svg>
+                <span>Play</span>
+              </button>
+              <div className="player__name">Transpotting</div>
+
+              <button type="button" className="player__full-screen">
+                <svg viewBox="0 0 27 27" width="27" height="27">
+                  <use xlinkHref="#full-screen"></use>
+                </svg>
+                <span>Full screen</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
